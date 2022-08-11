00241b81e360c47e3604a327ea7cf839d955cabd
Thipok Cholsaipant <thipok.ch@gmail.com>
2022-08-11 18:11:15 +0700

    build: environment configs
    build: versioning scripts
    build: codesign via fastlane match

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:method/main.dart';
+import 'package:method/main_dev.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
